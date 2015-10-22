7ee221cdc035424d249298a3c2130acdb774d865
Kathy Ems <luckykat09@gmail.com>
2015-10-21 18:45:55 -0700

    clean up unused files

--- testDB.php
+++
@@ -1,14 +0,0 @@
-<html>
-// 4/27/2015
-// Authored by Kathy Ems<head>
-<head>
-</head>
-<body> <?phpdate_default_timezone_set('America/Los_Angeles');// ~~~~~~~~~~Make a MySQL Connection~~~~~~~
-$con = mysql_connect('HOSTNAME','phoebe','bubbles2015');
-mysql_select_db('testMySQL', $con);
-?>
-
-
-mysql_free_result($result); //clears $results for saving memorymysql_close($con); // Close DB connection
-</body>
-</html>
