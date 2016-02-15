62b51838951a2c019eaf2a39dd09af29a9546164
Kathy Ems <redsangria@gmail.com>
2016-02-15 14:53:10 -0800

    add main.tpl.html to entry-app and index.ejs; add error.ejs

--- public/entry-app.js
+++ public/entry-app.js
@@ -1,21 +1,23 @@
-// (function () {
-//
-//     angular.module('adventure-wear', ['ngAnimate', 'ngRoute']);
-//
-//     function config ($routeProvider) {
-//       $routeProvider
-//         .when('/', {
-//           templateUrl: '../public/templates/main.tpl.html',
-//           controller: 'mainController',
-//           controllerAs: 'vm'
-//         })
-//         .otherwise({ redirectTo: '/' });
-//         // use the HTML5 History API
-//         $locationProvider.html5Mode(true);
-//       }
-//
-//     angular
-//       .module('adventure-wear')
-//       .config(['$routeProvider', config]);
-//
-//   })();
+var main = require('../public/templates/main.tpl.html');
+
+(function () {
+
+    angular.module('adventure-wear', ['ngAnimate', 'ngRoute']);
+
+    function config ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: '../public/templates/main.tpl.html',
+          controller: 'mainController',
+          controllerAs: 'vm'
+        })
+        .otherwise({ redirectTo: '/' });
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+      }
+
+    angular
+      .module('adventure-wear')
+      .config(['$routeProvider', config]);
+
+  })();
--- public/routes/routes.js
+++ public/routes/routes.js
@@ -1,15 +1,15 @@
-// module.exports = function($routeProvider, $locationProvider){
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: '../public/templates/main.tpl.html',
-//       controller: 'mainController'
-//     })
-//     .when('/wardrobe', {
-//       templateUrl: '../public/templates/wardrobe.tpl.html',
-//       controller: 'wardrobeController'
-//     })
-//     .otherwise({
-//       redirectTo:'/'
-//     });
-//   $locationProvider.html5Mode(true);
-// };
+module.exports = function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '../public/templates/main.tpl.html',
+      controller: 'mainController'
+    })
+    .when('/wardrobe', {
+      templateUrl: '../public/templates/wardrobe.tpl.html',
+      controller: 'wardrobeController'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+  $locationProvider.html5Mode(true);
+};
