60ed95bd1fe78a56754fa07fb186bd49e83f233c
Kathy Ems <redsangria@gmail.com>
2016-02-15 13:40:54 -0800

    populates controller with database

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,52 +1,42 @@
 (function () {
   var mainController = function ($scope) {
-  $scope.data = {
-    languages: [{
-      name: 'VBScript',
-      birthYr: 1996,
-      webInterface: 3,
-    },
-    {
-      name: 'JavaScript',
-      birthYr: 1995,
-      webInterface: 3
-    },
-    {
-      name: 'Ruby',
-      birthYr: 1995,
-      webInterface: 2
-    },
-    {
-      name: 'PHP',
-      birthYr: 1995,
-      webInterface: 2
-    },
-    {
-      name: 'JAVA',
-      birthYr: 1995,
-      webInterface: 2
-    },
-    {
-      name: 'Perl',
-      birthYr: 1987,
-      webInterface: 2
-    },
-    {
-      name: 'ActionScript',
-      birthYr: 1998,
-      webInterface: 1
-    },
-    {
-      name: 'Typescript',
-      birthYr: 2012,
-      webInterface: 1
-    },
-    {
-      name: 'Python',
-      birthYr: 1991,
-      webInterface: 1
-    }]
-  }
-};
-
-  })();
+    $scope.data = {
+      wardrobe: [{
+        top: 't-shirt',
+        bottom: 'capris',
+        head: 'sunglasses',
+        temperature: 56,
+        conditions: 'sunny',
+        humidity: 70,
+        activity: 'running'
+      },
+      {
+        top: 't-shirt',
+        bottom: 'pants',
+        head: 'headband',
+        temperature: 49,
+        conditions: 'cloudy',
+        humidity: 75,
+        activity: 'running'
+      },
+      {
+        top: 'long sleeved shirt',
+        bottom: 'pants',
+        head: 'headband',
+        temperature: 44,
+        conditions: 'cloudy',
+        humidity: 50,
+        activity: 'running'
+      },
+      {
+        top: 'tank',
+        bottom: 'shorts',
+        head: 'hat',
+        temperature: 72,
+        conditions: 'sunny',
+        humidity: 50,
+        activity: 'trail running'
+      }]
+    }
+  };
+})();
