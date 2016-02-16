9a422318b4c85d18e21ddb5146b8cc01acc35b73
Kathy Ems <redsangria@gmail.com>
2016-02-16 14:55:32 -0800

    Adds a filter for within 10 degrees of desired temp

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,12 +1,28 @@
 angular
   .module('adventurewear.main', [])
-    .controller('mainController', mainController);
+    .controller('mainController', mainController)
+    .filter('filterTemp', function() {
+    return function(input, desiredTemp) {
+      var out = [];
+      if (isNaN(desiredTemp)) {
+        out.push(desiredTemp);
+      } else {
+        angular.forEach(input, function(wardrobe) {
+          if (wardrobe.temperature > (desiredTemp-10) && wardrobe.temperature < (10 + desiredTemp) ) {
+            out.push(wardrobe);
+          }
+        })
+      }
+      return out;
+    }
+  });
 
-mainController.$inject = ['$scope'];
+mainController.$inject = ['$scope', '$filter'];
 
 function mainController ( $scope ) {
   $scope.data = {};
   $scope.data.wardrobe = {};
+
   $scope.data.pageHeader = {
     title: 'Adventure Wear!',
     tagline: "Pick an outfit! Your Adventure Awaits!"
@@ -50,49 +66,8 @@ function mainController ( $scope ) {
         humidity: 50,
         activity: 'trail running'
       }
-
-
     ]
   }
-
-  // $scope.data.wardrobe = {
-  //     wardrobe: [{
-  //     top: 't-shirt',
-  //     bottom: 'capris',
-  //     head: 'sunglasses',
-  //     temperature: 56,
-  //     conditions: 'sunny',
-  //     humidity: 70,
-  //     activity: 'running'
-  //   },
-  //   {
-  //     top: 't-shirt',
-  //     bottom: 'pants',
-  //     head: 'headband',
-  //     temperature: 49,
-  //     conditions: 'cloudy',
-  //     humidity: 75,
-  //     activity: 'running'
-  //   },
-  //   {
-  //     top: 'long sleeved shirt',
-  //     bottom: 'pants',
-  //     head: 'headband',
-  //     temperature: 44,
-  //     conditions: 'cloudy',
-  //     humidity: 50,
-  //     activity: 'running'
-  //   },
-  //   {
-  //     top: 'tank',
-  //     bottom: 'shorts',
-  //     head: 'hat',
-  //     temperature: 72,
-  //     conditions: 'sunny',
-  //     humidity: 50,
-  //     activity: 'trail running'
-  //   }];
-  // };
 };
 
 // WardrobeData.getWardrobe().then( function ( resp ) {
