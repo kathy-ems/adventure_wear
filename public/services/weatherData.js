d8bc5c36a8fe5575d127d84fadcf948257dd9256
Kathy Ems <redsangria@gmail.com>
2016-03-11 19:06:09 -0800

    updates database data; adds local host console log for users

---
+++ public/services/weatherData.js
@@ -0,0 +1,12 @@
+angular
+  .module('adventurewear.weatherData', [])
+    .factory('WeatherData', function () {
+
+      var getWardrobe = function () {
+        return {
+
+// http://api.wunderground.com/api/0852f8aa992c2889/conditions/q/CA/San_Francisco.json
+
+        }
+      }
+    });
