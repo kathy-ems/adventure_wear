d189976db322fe5838310eec93a70a9e4b3fbbaa
Kathy Ems <redsangria@gmail.com>
2016-03-26 09:00:17 -0700

    fixes console log errors

--- public/common/filters.js
+++ public/common/filters.js
@@ -16,7 +16,7 @@ angular
       if (
         wardrobe.temperature >= (search.temp-2)
         && wardrobe.temperature <= (search.temp + 2)
-        && wardrobe.activity.toLowerCase() === search.activity.toLowerCase()
+        && wardrobe.activity === search.activity.toLowerCase()
       ) {
         out.push(wardrobe);
       }
@@ -35,25 +35,26 @@ angular
 })
 .filter('titleCase', function() {
   return function (input) {
-    if(input === "N/A"){
-      return input;
+    if(input === "N/A" || input === undefined){
+      return "N/A";
     }
-      var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-
+    var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+    if(input !== null) {
       input = input.toLowerCase();
       return input.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title) {
-          if (index > 0 && index + match.length !== title.length &&
-              match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-              (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-              title.charAt(index - 1).search(/[^\s-]/) < 0) {
-              return match.toLowerCase();
-          }
+        if (index > 0 && index + match.length !== title.length &&
+            match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+            (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+            title.charAt(index - 1).search(/[^\s-]/) < 0) {
+            return match.toLowerCase();
+        }
 
-          if (match.substr(1).search(/[A-Z]|\../) > -1) {
-              return match;
-          }
+        if (match.substr(1).search(/[A-Z]|\../) > -1) {
+            return match;
+        }
 
-          return match.charAt(0).toUpperCase() + match.substr(1);
+        return match.charAt(0).toUpperCase() + match.substr(1);
       });
+    }
   }
 });
